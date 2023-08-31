f541ac4e2b5967c7915e2249a1c01a5ae9f19638
Amy Lee <39871117+amchrlee@users.noreply.github.com>
2023-08-31 18:29:44 -0400

    Create function to insert trash icon in remove btn

--- script.js
+++ script.js
@@ -69,12 +69,9 @@ function createCard(book) {
 
     title.textContent = `"${book.title}"`;
     author.textContent = book.author;
-    totalPages.textContent = book.totalPages;
+    totalPages.textContent = book.totalPages;    
     removeBtn.textContent = 'Remove';
-
-    // const svgIcon = '<div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg></div>';
-    
-    // removeBtn.outerHTML += svgIcon;
+    linkTrashIcon(removeBtn);
 
     if(book.readStatus === "Read") {
         readStatusBtn.textContent = "Read";
@@ -94,4 +91,18 @@ function createCard(book) {
     btnContainer.appendChild(readStatusBtn);
     btnContainer.appendChild(removeBtn);
     bookContainer.appendChild(bookCard);
+}
+
+function linkTrashIcon(node) {
+    const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+    iconSvg.setAttribute("viewBox","0 0 24 24");
+    iconPath.setAttribute(
+        "d", 
+        "M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z"
+    );
+    
+    iconSvg.appendChild(iconPath);
+    return node.appendChild(iconSvg);
 }
